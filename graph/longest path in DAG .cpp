bd3ecb744c546d431deff28a4f1cfdef22ae685a
sagsango <34793802+sagsango@users.noreply.github.com>
2019-06-16 03:27:49 +0530

    Create longest path in DAG .cpp

---
+++ graph/longest path in DAG .cpp
@@ -0,0 +1,11 @@
+/*
+
+
+UCSD string processing and pattern matching
+1. do topological sort on grath
+   now all the vectex length will we depends on
+   Lmax[ v ] = max(  W[ v ][ u1 ] + Lmax[ u1 ]  ,  W[ v ][ u2 ] + Lmax[ u2 ] , ...    )
+   u1 , u2 .... are adjecnt of v having lower topological value
+   its privious verticies length
+2. Like a dp we can process one by one 
+   we will fill adject having heigher topological value accordingly max.

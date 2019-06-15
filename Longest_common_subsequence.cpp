bb7a0c7a068ca47a78d381e1abfaf2e874c9dd48
sagsango <34793802+sagsango@users.noreply.github.com>
2019-06-16 02:45:36 +0530

    Create Longest_common_subsequence.cpp

---
+++ Longest_common_subsequence.cpp
@@ -0,0 +1,74 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+#define pb push_back
+#define srt(a) sort(a.begin(),a.end())
+#define rsrt(a) sort(a.rbegin(),a.rend())
+#define lb(a,b) lower_bound(a.begin(),a.end(),b)
+#define ub(a,b) upper_bound(a.begin(),a.end(),b)
+#define MOD ((int)(1e9+7))
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define int long long
+#define float long double
+#define sqrt sqrtl
+#define cbrt cbrtl
+typedef vector<int> vi;
+typedef vector<float> vf;
+typedef pair<float,int> fi;
+typedef pair<int,int> ii;
+typedef vector<ii> vii;
+typedef vector<fi> vfi;
+typedef vector<vi> vvi;
+typedef vector<vf> vvf;
+typedef vector<vii> vvii;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+typedef priority_queue<int> pqi;
+typedef priority_queue<ii> pqii;
+typedef priority_queue<fi> pqfi;
+typedef priority_queue <int, vector<int>, greater<int> > rpqi; 
+typedef priority_queue <ii, vector<ii>, greater<ii> > rpqii; 
+typedef priority_queue <fi, vector<fi>, greater<fi> > rpqfi; 
+
+
+//KNP
+int32_t main()
+{
+	IOS
+	string s1;
+	string s2;
+
+	cin>>s1>>s2;
+	int n1=s1.length();
+	int n2=s2.length();
+	s1='$'+s1;
+	s2='$'+s2;
+	
+	
+	vvi dp(n1+1,vi(n2+1));
+	for(int i=1;i<=n1;i++)
+	{
+		for(int j=1;j<=n2;j++)
+		{
+			if(s1[i]==s2[j])
+			{
+				dp[i][j]=max({dp[i-1][j-1] + 1 ,dp[i-1][j] ,dp[i][j-1] });
+			}
+			else
+			{
+				dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+			}
+		}
+	}
+	cout<<dp[n1][n2]<<endl;
+
+	
+	
+	
+}
+
+			

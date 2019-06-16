9b6623bc5dd3d1c43d73daf91155795688665f5c
sagsango <34793802+sagsango@users.noreply.github.com>
2019-06-16 18:41:14 +0530

    Update Ternary search in Unimodal  function  .cpp

--- Binary Search Applications/Ternary search in Unimodal  function  .cpp
+++ Binary Search Applications/Ternary search in Unimodal  function  .cpp
@@ -6,9 +6,9 @@ https://en.wikipedia.org/wiki/Ternary_search
 int m1 = l + (r-l)/3 ;
 int m2 = r - (r-l)/3 ;
 
-if( arr[m1]  <  arr[m2] ) update result = arr[m2]  and search in [m1+1,r]
-if( arr[m1]  >  arr[m2] ) update result = arr[m1]  and search in [l,m2-1]
-if( arr[m1] ==  arr[m2] ) update result = arr[m1]  and search in [m1+1,m2-1]
+if( f(m1)  <  f(m2) ) update result = arr[m2]  and search in [m1+1,r]
+if( f(m1)  >  f(m2) ) update result = arr[m1]  and search in [l,m2-1]
+if( f(m1) ==  f(m2) ) update result = arr[m1]  and search in [m1+1,m2-1]
 
 we will combine 3rd case in any one of the above two
 
@@ -17,3 +17,44 @@ we will combine 3rd case in any one of the above two
 */
 
 
+//recursive
+int input;
+void ternary_search(int l,int r)
+{
+    if( l > r )return ;
+    int m1 = l + (r-l)/3;
+    int m2 = r - (r-l)/3;
+    
+    if( f(m1) < f(m2) )
+    {
+         input=m2;
+         l=m1+1;
+    }
+    else
+    {
+         input=m1;
+         r=m2-1;
+    }
+    ternary_search(l,r);
+}
+
+
+//iterative
+int l=1,r=n,input;
+while( l <= r)
+{
+    int m1 = l + (r - l)/3;
+    int m2 = r - (r - l)/3;
+    
+   if( f(m1) < f(m2) )
+    {
+         input=m2;
+         l=m1+1;
+    }
+    else
+    {
+         input=m1;
+         r=m2-1;
+    }
+}
+      

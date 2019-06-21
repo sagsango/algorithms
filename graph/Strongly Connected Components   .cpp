d83153b2eebcc4ed5d1a02691b51f026096c19ad
sagsango <34793802+sagsango@users.noreply.github.com>
2019-06-22 02:23:52 +0530

    Create Strongly Connected Components   .cpp

---
+++ graph/Strongly Connected Components   .cpp
@@ -0,0 +1,6 @@
+In directed graph set of verticies which are reachable from eath others , forms strongly connected components.
+
+we reverse the direction of the edges .
+then do dfs with storing their postvisit numbers.
+the sort them in reverse order of their postvisit number.
+then perform dfs to get connected components there r strongly conncted components.

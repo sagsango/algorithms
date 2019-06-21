3818def4fd8abd048901527f39710b4bb0826474
sagsango <34793802+sagsango@users.noreply.github.com>
2019-06-22 02:16:50 +0530

    Create Topological sorting .cpp

---
+++ graph/Topological sorting .cpp
@@ -0,0 +1,8 @@
+
+
+privisit[ v  ]  =  time when we r going to start all the neighbours of v.
+postvisit[ v ]  =  time when we have finished exploration all the neighbours of v.
+
+
+if we sort the verticies in reverse order of their postvisit number then it will form a possible topological sort.
+because sources have greater postvisit number and sinc have smaller postvisit number.

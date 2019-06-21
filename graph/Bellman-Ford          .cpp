07bbea8d3729bc138d02b355ad9d609644f39d2f
sagsango <34793802+sagsango@users.noreply.github.com>
2019-06-22 03:10:04 +0530

    Create Bellman-Ford          .cpp

---
+++ graph/Bellman-Ford          .cpp
@@ -0,0 +1,23 @@
+Bellman-Ford
+Bellman-Ford is an algorithm for single source shortest path where edges can be negative (but if there is a cycle with negative weight, then this problem will be NP).
+
+The main idea is to relax all the edges exactly n - 1 times (read relaxation above in dijkstra). You can prove this algorithm using induction.
+
+If in the n - th step, we relax an edge, then we have a negative cycle (this is if and only if).
+
+Code :
+
+Bellman-Ford(int v)
+	d[i] = inf for each vertex i
+	d[v] = 0
+	for step = 1 to n
+		for all edges like e
+			i = e.first // first end
+			j = e.second // second end
+			w = e.weight
+			if d[j] > d[i] + w
+				if step == n
+					then return "Negative cycle found"
+				d[j] = d[i] + w
+Time complexity : O(nm).
+

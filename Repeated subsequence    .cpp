901432670336052240735f8eef748b088b242b84
sagsango <34793802+sagsango@users.noreply.github.com>
2019-08-03 12:08:56 +0530

    Create Repeated subsequence    .cpp

---
+++ Repeated subsequence    .cpp
@@ -0,0 +1,11 @@
+//given a string find any repeated subsequence of length greater than 1 present or not
+//https://www.techiedelight.com/check-repeated-subsequence-present-string-not/
+
+XYBAXB =>XB,XB
+XBXAXB =>XX,XX
+ABCABA => No
+
+1.remove the char which apperared only once
+2.check string is plindrome or not
+3.If string is palindrom and all char have frequency less than 3 then there is no repeated subsequence
+4.Otherwise there is a repeated subsequence 

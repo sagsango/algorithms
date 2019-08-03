aad37b466b84b7f2558e06ef289640ed7476d294
sagsango <34793802+sagsango@users.noreply.github.com>
2019-08-03 11:30:48 +0530

    Update Pattern as subsequence    .cpp

--- string/Pattern as subsequence    .cpp
+++ string/Pattern as subsequence    .cpp
@@ -57,3 +57,26 @@ void fun(string s1,int i1,string s2,int i2)
 	}
 }
 
+
+//recursive dp
+int pattren_as_subsequence(string s1,string s2)
+{
+	int n1=s1.length();
+	int n2=s2.length();
+	vector<vector<int>>dp(n1+1,vector<int>(n2+1));
+	for(int i=0;i<=n1;i++)dp[i][0]=1;
+	s1='$'+s1;
+	s2='$'+s2;
+	for(int i=1;i<=n1;i++)
+	{
+		for(int j=1;j<=n2;j++)
+		{
+			if(s1[i]==s2[j])
+			{
+				dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
+			}
+			else dp[i][j]=dp[i-1][j];
+		}
+	}
+	return dp[n1][n2];
+}

280e311bf766bd27b9327f6a13b1318cee0fd617
sagsango <34793802+sagsango@users.noreply.github.com>
2019-10-07 21:29:08 +0530

    Create pattern searching.cpp

---
+++ string/KMP/pattern searching.cpp
@@ -0,0 +1,4 @@
+Given a text t and a string s, we want to find and display the positions of all occurrences of the string s in the text t.
+find prefix function of string s+'#'+t
+in pi when pi[i]==s.length() then it is the last index of matched substring which is equal to s
+because pi[i]:will be the longest prifix which is also suffix.

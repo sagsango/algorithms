eda61fc5dd9d6a2d8442ed65081252fd11bf6d9f
sagsango <34793802+sagsango@users.noreply.github.com>
2019-10-07 18:32:26 +0530

    Create longest palindromic subsequence.cpp

---
+++ string/DP/longest palindromic subsequence.cpp
@@ -0,0 +1,3 @@
+The longest plindromic subsequence problem is classic variation of 
+Longest Common Subsequence [LCS] problem. The idea is to find LCS of given string
+with its revers and longest common subsequence will be the longest palindromic subsequence

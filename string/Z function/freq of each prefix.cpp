b47e1fe24b8ca174a4fdd402b644d2b6a1d1ccde
sagsango <34793802+sagsango@users.noreply.github.com>
2019-10-08 02:12:35 +0530

    Create freq of each prefix.cpp

---
+++ string/Z function/freq of each prefix.cpp
@@ -0,0 +1,22 @@
+O(n + n)
+O(n) :z_function
+O(n) :freq_of_each_prefix
+
+vector<int> freq_of_each_prefix(string s)
+{ 
+  vector<int>z=z_function(s);
+	z[0]=n;//in our discussion we had taken z[0]=0
+	vector<int>freq(n+2);
+	for(int i=0;i<n;i++)
+	if(z[i])
+	{
+		freq[z[i]+1]-=1;
+		freq[1]+=1;
+	}
+	for(int i=1;i<=n;i++)
+	freq[i]+=freq[i-1];
+  return freq;
+}
+/**
+* freq[i] : frequency of the prifix having length i
+**/

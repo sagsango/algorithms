17f56ceb656a0d1ee09dfa321b4955c5b2ab6ca6
sagsango <34793802+sagsango@users.noreply.github.com>
2019-10-07 17:55:57 +0530

    Update Longest Common Substring.cpp

--- string/DP/Longest Common Substring.cpp
+++ string/DP/Longest Common Substring.cpp
@@ -16,8 +16,8 @@ int32_t main()
 	
 	
 	vvi dp(n1+1,vi(n2+1));
-  int max_length=0;
-  int sub_index_end=-1;
+        int max_length=0;
+        int sub_index_end=-1;
 	for(int i=1;i<=n1;i++)
 	{
 		for(int j=1;j<=n2;j++)
@@ -25,14 +25,14 @@ int32_t main()
 			if(s1[i]==s2[j])
 			{
 				dp[i][j]=dp[i-1][j-1] + 1;
-        if(dp[i][j]>max_length)
-        {
-          max_length=dp[i][j];
-          sub_index_end=i;//Indexing from  1
-        }
+                                if(dp[i][j]>max_length)
+                                {
+                                        max_length=dp[i][j];
+                                        sub_index_end=i;//Indexing from  1
+                                }
 			}
-		
 		}
+			
 	}
 	cout<<max_length<<" "<<s1.substr(sub_index_end-max_length+1,max_length)<<endl;
 

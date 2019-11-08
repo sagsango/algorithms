dc3010527818c8ddd5de312e6a97c684c0601432
sagsango <34793802+sagsango@users.noreply.github.com>
2019-11-08 13:20:55 +0530

    Create lighter_edges_given.cpp

---
+++ Hidden Edge w1,w2 MST/lighter_edges_given.cpp
@@ -0,0 +1,5 @@
+find total no of the components using only lighter edges
+     calculate lighter waight needed for every component
+havier waight edged will be eqal to [no of components] -1;
+
+https://codingcompetitions.withgoogle.com/kickstart/round/0000000000050edb/0000000000170721

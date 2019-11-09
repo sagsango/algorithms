abb990317c6828457b806552c16d64c478dbb253
sagsango <34793802+sagsango@users.noreply.github.com>
2019-11-10 02:17:51 +0530

    Delete eulerian 2.cpp

--- graph/Eulerian Tours Magic/eulerian 2.cpp
+++
@@ -1,9 +0,0 @@
-vector<int> euler;
-void dfs(int v,int p = -1){
-	euler.push_back(v);
-	for(auto u : adj[v])	if(p - u)
-		dfs(u,v), euler.push_back(v);
-}
-
-
-https://codeforces.com/blog/entry/16221

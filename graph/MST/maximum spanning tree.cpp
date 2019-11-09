ca8dbe368031b8971b868786e940dd892b8c10aa
sagsango <34793802+sagsango@users.noreply.github.com>
2019-11-09 22:54:27 +0530

    Create maximum spanning tree.cpp

---
+++ graph/MST/maximum spanning tree.cpp
@@ -0,0 +1,4 @@
+A maximum spanning tree is a spanning tree of a weighted graph having maximum weight.
+You can compute maximum spanning tree using Kruskal Algorithm with edges sorted into decreasing order.
+
+The same thing as an algorithm to find the Minimum spanning tree (after you multiplied the cost of each edge by -1).

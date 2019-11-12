91ebf5aaff46999ad25499d3e6ccd0fbc0e94548
sagsango <34793802+sagsango@users.noreply.github.com>
2019-11-12 14:24:16 +0530

    Create resources.cpp

---
+++ graph/Seg tree or Dsu or BIT on trees/Heavy Light Decomposition/resources.cpp
@@ -0,0 +1,8 @@
+https://codeforces.com/blog/entry/22072
+https://codeforces.com/blog/entry/12239
+https://codeforces.com/blog/entry/12488
+https://codeforces.com/blog/entry/12579
+https://codeforces.com/blog/entry/65924
+.....
+....
+search

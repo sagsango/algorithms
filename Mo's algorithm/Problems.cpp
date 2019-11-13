b82fb379a51825598e344e2cc626cdd9aa91c921
sagsango <34793802+sagsango@users.noreply.github.com>
2019-11-13 12:47:18 +0530

    Create Problems.cpp

---
+++ Mo's algorithm/Problems.cpp
@@ -0,0 +1,4 @@
+https://codeforces.com/blog/entry/46399?#comment-308528
+https://www.spoj.com/problems/COT2/
+https://codeforces.com/gym/100962/attachments/download/4255/20152016-petrozavodsk-winter-training-camp-moscow-su-trinity-contest-en.pdf
+https://www.codechef.com/ALKH2016/problems/VLB

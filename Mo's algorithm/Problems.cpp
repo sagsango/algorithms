c2be381404a950e333ff71df0bcfe453982046b1
sagsango <34793802+sagsango@users.noreply.github.com>
2019-11-13 18:59:36 +0530

    Update Problems.cpp

--- Mo's algorithm/Problems.cpp
+++ Mo's algorithm/Problems.cpp
@@ -2,3 +2,4 @@ https://codeforces.com/blog/entry/46399?#comment-308528
 https://www.spoj.com/problems/COT2/
 https://codeforces.com/gym/100962/attachments/download/4255/20152016-petrozavodsk-winter-training-camp-moscow-su-trinity-contest-en.pdf
 https://www.codechef.com/ALKH2016/problems/VLB
+see contest of "Mos Algorithm" in Gym

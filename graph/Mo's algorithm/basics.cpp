5c9ea00878b4762c48eeaf29c8cab02034832b66
sagsango <34793802+sagsango@users.noreply.github.com>
2019-11-15 00:50:38 +0530

    Update basics.cpp

--- graph/Mo's algorithm/basics.cpp
+++ graph/Mo's algorithm/basics.cpp
@@ -9,5 +9,138 @@ Solution: After flatterning the tree
           p=par(u,v)
           1.p==u or p==v  : process [ in[u] , in[v] ] Range
           2.p!=u or p!=v  : process [ out[u], in[v] ] +  [ in[p] ,in[p] ] Range
-          
+   
+   
 
+example     :    https://www.spoj.com/problems/COT2/
+statement   :    You are given a tree with N nodes. The tree nodes are numbered from 1 to N. Each node has an integer colour.
+                 We will ask you to perform the following operation:
+                 u v : ask for how many different colour on the path from u to v.
+solution    :
+
+
+
+#include<bits/stdc++.h>
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+using namespace std;
+ 
+struct query
+{
+    int l,r,k,i;
+};
+const int S=283;
+bool cmp(query A, query B){
+  if (A.l / S != B.l / S) return A.l < B.l;
+  return A.l / S % 2 ? A.r > B.r : A.r < B.r;
+}
+ 
+const int N=4e4;
+vector<int>arr(2*N+1);
+vector<short int>parity(N+1);
+unordered_map<int,int>cnt;
+vector<int>clr(N+1);
+vector<int>tin(N+1);
+vector<int>tout(N+1);
+vector<int>g[N+1];
+int n,m,l=1,r=1,tc=0,sum=0;
+const int L=16;
+vector<vector<int>>up(N+1,vector<int>(L+1));
+ 
+void dfs(int u,int p)
+{
+	tin[u]=++tc;arr[tin[u]]=u;
+	up[u][0]=p;
+	for(int i=1;i<=L;i++)
+	up[u][i]=up[ up[u][i-1] ][i-1];
+	for(auto v:g[u])if(v!=p)dfs(v,u);
+	tout[u]=++tc;arr[tout[u]]=u;
+}
+bool is_ancestor(int u,int v){return tin[u]<=tin[v] && tout[v]<=tout[u];}
+int lca(int u,int v)
+{
+	if(is_ancestor(u,v))return u;
+	if(is_ancestor(v,u))return v;
+	for(int i=L;i>=0;i--)
+	if(up[u][i] && !is_ancestor(up[u][i],v))
+	u=up[u][i];
+	return up[u][0];
+}
+/**
+ * Insert and Remove function are same 
+ * It took me 3 Hours to debug it
+ **/
+void insert(int i)
+{
+	int v=arr[i];
+	sum-=cnt[clr[v]]!=0;
+	parity[v]^=1;
+	if(parity[v])cnt[clr[v]]+=1;
+	else cnt[clr[v]]-=1;
+	sum+=cnt[clr[v]]!=0;
+}
+void remove(int i)
+{
+	int v=arr[i];
+	sum-=cnt[clr[v]]!=0;
+	parity[v]^=1;
+	if(parity[v])cnt[clr[v]]+=1;
+	else cnt[clr[v]]-=1;
+	sum+=cnt[clr[v]]!=0;
+}
+int32_t main()
+{
+	IOS	
+	cin>>n>>m;
+	for(int i=1;i<=n;i++)
+	cin>>clr[i];
+	for(int i=1;i<=n-1;i++)
+	{
+		int u,v;cin>>u>>v;
+		g[u].push_back(v);
+		g[v].push_back(u);
+	}
+	dfs(1,0);
+		
+	vector<query>q(m);
+	vector<int>ans(m);
+	for(int i=0;i<m;i++)
+	{
+		int u,v;cin>>u>>v;
+		int l,r,k,p=lca(u,v);
+	          if(p==u||p==v)
+	          {
+			if(tin[u] <= tin[v])l=tin[u],r=tin[v];
+			else l=tin[v],r=tin[u];
+			k=0;
+		}
+	          else
+		{
+			if(tout[u] < tin[v])l=tout[u],r=tin[v];
+			else l=tout[v],r=tin[u];
+			k=p;
+		}
+		q[i].l=l;
+		q[i].r=r;
+		q[i].k=k;
+		q[i].i=i;
+	}
+		
+	sort(q.begin(),q.end(),cmp);
+	insert(1);
+	for(int i=0;i<m;i++)
+	{
+		while(l > q[i].l)insert(--l);
+		while(r < q[i].r)insert(++r);
+		while(l < q[i].l)remove(l++);
+		while(r > q[i].r)remove(r--);
+ 
+                    if(q[i].k)insert(tin[q[i].k]);
+		ans[q[i].i]=sum;
+	          if(q[i].k)remove(tin[q[i].k]);
+	}
+	for(int i=0;i<m;i++)
+	cout<<ans[i]<<" ";
+	cout<<endl;
+	
+	
+}

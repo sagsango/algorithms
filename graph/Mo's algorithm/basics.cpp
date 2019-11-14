11dd109519bfeab337a88ed1f9b0c64b20d8d0c7
sagsango <34793802+sagsango@users.noreply.github.com>
2019-11-14 22:09:12 +0530

    Update basics.cpp

--- graph/Mo's algorithm/basics.cpp
+++ graph/Mo's algorithm/basics.cpp
@@ -1,7 +1,13 @@
 https://codeforces.com/blog/entry/43230
 https://blog.anudeep2011.com/mos-algorithm/
 
-See problem and solution
-https://www.spoj.com/problems/DQUERY/
 
+Explanation of Blog:
+Problem : Find no of distinct colour from path u to v
+Solution: After flatterning the tree
+          1, 2, 3, 4, 4, 3, 5, 6, 6, 5, 2, 7, 8, 8, 9, 9, 7, 1
+          p=par(u,v)
+          1.p==u or p==v  : process [ in[u] , in[v] ] Range
+          2.p!=u or p!=v  : process [ out[u], in[v] ] +  [ in[p] ,in[p] ] Range
+          
 

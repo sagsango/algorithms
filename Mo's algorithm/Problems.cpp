39a972baedd6980ec24f6af1c8e19c5f6468c3f1
sagsango <34793802+sagsango@users.noreply.github.com>
2019-11-14 18:51:24 +0530

    Update Problems.cpp

--- Mo's algorithm/Problems.cpp
+++ Mo's algorithm/Problems.cpp
@@ -2,4 +2,7 @@ https://codeforces.com/blog/entry/46399?#comment-308528
 https://www.spoj.com/problems/COT2/
 https://codeforces.com/gym/100962/attachments/download/4255/20152016-petrozavodsk-winter-training-camp-moscow-su-trinity-contest-en.pdf
 https://www.codechef.com/ALKH2016/problems/VLB
+https://www.spoj.com/problems/ZQUERY/
+https://www.hackerearth.com/challenges/competitive/july-clash-15/algorithm/something-genuine/
+https://www.hackerrank.com/contests/world-codesprint-9/challenges/box-operations
 see contest of "Mos Algorithm" in Gym

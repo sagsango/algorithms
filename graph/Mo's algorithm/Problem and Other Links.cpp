002475226823a7b405f58cf885c84e803501e8dd
sagsango <34793802+sagsango@users.noreply.github.com>
2019-11-15 01:26:57 +0530

    Update Problem and Other Links.cpp

--- graph/Mo's algorithm/Problem and Other Links.cpp
+++ graph/Mo's algorithm/Problem and Other Links.cpp
@@ -9,3 +9,17 @@ https://www.spoj.com/problems/COT2/
 https://codeforces.com/gym/100962/attachments/download/4255/20152016-petrozavodsk-winter-training-camp-moscow-su-trinity-contest-en.pdf
 https://www.codechef.com/ALKH2016/problems/VLB
 https://discuss.codechef.com/t/mlchef-editorial/3172
+
+http://cfrp.azurewebsites.net/blog/entry/62456
+https://codeforces.com/blog/entry/68271
+https://codeforces.com/blog/entry/50353
+https://codeforces.com/blog/entry/55574
+https://codeforces.com/blog/entry/7383
+https://codeforces.com/blog/entry/20032
+https://codeforces.com/blog/entry/44711
+https://codeforces.com/blog/entry/15890
+https://www.geeksforgeeks.org/mos-algorithm-query-square-root-decomposition-set-1-introduction/
+https://cp-algorithms.com/data_structures/sqrt_decomposition.html
+http://hmrockstar.blogspot.com/2017/07/mos-query-sqrt-decomposition-algorithm.html
+https://www.hackerearth.com/practice/notes/mos-algorithm/
+https://discuss.codechef.com/t/merge-sort-tree-tutorial/14277

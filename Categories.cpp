a5c5080ba0d864194a6d5eda84e60abb02e50dc4
Sagar Singh <34793802+sagsango@users.noreply.github.com>
2019-11-20 18:29:13 +0530

    Create Categories.cpp

---
+++ Categories.cpp
@@ -0,0 +1,850 @@
+
+A2 Online Judge»    
+Welcome sagsango
+Category name: 
+---------------------------------------------------------------------------------
+Category	Problems Count
+1	Dynamic Programming	1210
+2	implementation	1054
+3	math	679
+4	Greedy	599
+5	DFS & BFS & Dijkstra	565
+6	brute force	494
+7	data structures	489
+8	Codeforces Div. 2 - A Problems	476
+9	Codeforces Div. 2 - C Problems	460
+10	Codeforces Div. 2 - B Problems	457
+11	Codeforces Div. 2 - D Problems	443
+12	Codeforces Div. 2 - E Problems	439
+13	Best Problem Setters	410
+14	Binary Search & Ternary Search	350
+15	constructive algorithms	331
+16	sortings	316
+17	graphs	291
+18	Geometry	254
+19	Number Theory	251
+20	strings	216
+21	combinatorics	215
+22	Codeforces Div. 1, D	209
+23	trees	202
+24	Segment Tree	162
+25	Network Flow	159
+26	probabilities	152
+27	two pointers	128
+28	Codeforces Div. 2 - F Problems	119
+29	dsu	116
+30	bitmasks	113
+31	Problem list	100
+32	Matrix Power	90
+33	Top Coder	84
+34	games	81
+35	#Errichto	78
+36	MST,Disjoint sets,SCC	77
+37	hashing	76
+38	Binary Indexed Tree	69
+39	divide and conquer	66
+40	Ad-hoc	64
+41	CUET	56
+42	Maximum Matching / Vertex Cover on Bipartite Graph	50
+43	backtracking	42
+44	Trie	42
+45	bfs	37
+46	Heavy-Light Descomposition	36
+47	Non-Classical Dynamic Programming	36
+48	KMP	35
+49	string suffix structures	35
+50	expresison and parsing	33
+51	Meet in The Middle	29
+52	0/1 Knapsack	27
+53	Suffix Array/Tree	27
+54	Balanced Binary Search Trees	26
+55	The_scorpion	24
+56	MOs Algorithm (Query square root decomposition)	24
+57	FFT	23
+58	Convex hull	22
+59	LIS	21
+60	TopologicalSort	21
+61	2sat	21
+62	ACM-ICPC Dhaka Site Regional Contests	20
+63	polynomial	20
+64	All Pair Shortest Path (Floyd-Warshall)	18
+65	LCS	17
+66	Aho-Corasick	16
+67	LCA	16
+68	Codeforces Practice List	15
+69	interactive	15
+70	Strongly Connected Components	15
+71	EulerianTour	15
+72	Centroid Decomposition	14
+73	Suffix automaton	14
+74	back-tracking	14
+75	Lowest Common Ancestor	14
+76	Reasoning	13
+77	line sweep	13
+78	Hungarian Algorithm	13
+79	Gaussian elimination	11
+80	Mobius Function	10
+81	Coin Change	10
+82	beginner	10
+83	DFS&BFS	10
+84	Strategy	10
+85	Edit Distance	9
+86	STL	9
+87	TowerOfHanoi	9
+88	ad hoc	9
+89	Persistent Segment Tree	9
+90	Numerical_Integration	9
+91	Sqrt decomposition	9
+92	Math Games	9
+93	Articulation Point	8
+94	bit manipulation	8
+95	Randomized Algorithm	8
+96	Heaps	8
+97	Dijkstra	8
+98	istasis likes dhinchak pooja	8
+99	Complete Searsh	8
+100	Simulated Annealing	7
+101	segment trees	7
+102	MCM & OBST	7
+103	Bellman-Ford	6
+104	BinarySearchVariants	6
+105	big numbers	6
+106	0/1 BFS	6
+107	STOP ADDING DUMP CATEGORIES	6
+108	RMQ	6
+109	Simulation	6
+110	digit dp	6
+111	chinese remainder theorem	6
+112	Square Root Decomposition	6
+113	pc	5
+114	Minimum Cost Flow	5
+115	Recursion	5
+116	Rabin-Karp	5
+117	ETF	5
+118	Minimum Spanning Tree	5
+119	Game Theory	5
+120	kth shortest path	5
+121	Treap	5
+122	matrix exponentiation	5
+123	Optimization	5
+124	2D Range Queries	5
+125	SGU Problems	5
+126	AI	4
+127	Iniciante	4
+128	Partitioning	4
+129	Graph basics concepts BFS & DFS	4
+130	tries	4
+131	schedules	4
+132	Grid compression	4
+133	Kadane's Algorithm	4
+134	dinamyc porograming	4
+135	Difference Constraints	3
+136	Newton Raphson method	3
+137	Range tree	3
+138	Minkowski addition	3
+139	flood fill	3
+140	Prefix Sums	3
+141	Z algorithm	3
+142	Dynamic Programmin	3
+143	Float precision	3
+144	binary search	3
+145	linked list	3
+146	acm	2
+147	dfs	2
+148	SQRT Heuristic	2
+149	Polygon centroid	2
+150	Linear Programming	2
+151	Minimum Enclosing Circle	2
+152	ista breaks up with shreya	2
+153	justSama	2
+154	string	2
+155	MST	2
+156	frequency ARRAY	2
+157	SRM 557 DivII 250	2
+158	MYTE	2
+159	data structure	2
+160	big mod	2
+161	Max points on line	2
+162	binar search	2
+163	Algorithm X	2
+164	Factoradic Index	2
+165	Biconnected Component	2
+166	maxflow	2
+167	Alignment	2
+168	Voronoi	2
+169	dp	2
+170	bst	2
+171	Pigeonhole principle	2
+172	dfs & bfs	1
+173	prime	1
+174	Arrays	1
+175	gcd	1
+176	matrix multiplication problem	1
+177	Minimum cost arborescence	1
+178	algorithms	1
+179	Basica	1
+180	dynammic programming	1
+181	FST Test	1
+182	Logic	1
+183	maximum empty rectangle	1
+184	flow	1
+185	Mathematical Questions	1
+186	Codeforces	1
+187	binary search tree	1
+188	square	1
+189	kingdon	1
+190	topological	1
+191	contest	1
+192	Circle sweep	1
+193	Queues	1
+194	ECPC 2014	1
+195	begainners	1
+196	disunion set	1
+197	Dfs and similar	1
+198	binary searcj	1
+199	kruskal	1
+200	Coordinate Compression	1
+201	uva	1
+202	algorithm	1
+203	ista = LGBT head	1
+204	kmp algo	1
+205	Dynam	1
+206	Disjoint Set Union	1
+207	mathematical	1
+208	uri	1
+209	bigmod	1
+210	omar	1
+211	109	1
+212	link	1
+213	brute	1
+214	acm egypt	1
+215	vectors	1
+216	sieve	1
+217	Begginer	1
+218	general	1
+219	Stable Marriage	1
+220	scc	1
+221	ciclos	1
+222	SRP_HOMEWORK_01	1
+223	bi connected component	1
+224	suffix array	1
+225	fenwick tree	1
+226	frequency	0
+227	graph	0
+228	IIIT-H DP	0
+229	div 2C	0
+230	level 1	0
+231	level 2	0
+232	codeforce	0
+233	linke	0
+234	dilworth	0
+235	amman cpc 2015	0
+236	cf	0
+237	pst0	0
+238	modular arithmatic	0
+239	codechef	0
+240	grid	0
+241	convert time	0
+242	queue	0
+243	power	0
+244	dag	0
+245	con	0
+246	C#	0
+247	28239	0
+248	dpinto	0
+249	number throry level 1	0
+250	matrices	0
+251	knapsack	0
+252	stl map	0
+253	aho	0
+254	closest pair of point	0
+255	coin row	0
+256	indonesia	0
+257	arraya	0
+258	bitset	0
+259	loop	0
+260	beginners	0
+261	bipartite matching	0
+262	xor	0
+263	Mispelling	0
+264	recursion'	0
+265	Google Code Jam	0
+266	codeforces dp	0
+267	1c	0
+268	codeforces DIv.2A	0
+269	andres	0
+270	hare and tortoise	0
+271	stable roomate	0
+272	seg	0
+273	p'[	0
+274	centroid	0
+275	problem set	0
+276	div1	0
+277	implemntation	0
+278	binary searsh	0
+279	mo's algo	0
+280	andrerabelo	0
+281	merge sort	0
+282	so easy	0
+283	DP Codeforces	0
+284	heavy light decomposition	0
+285	ACM Syrian Collegiate Programming Contest 2	0
+286	sdasd	0
+287	sort drinking	0
+288	prefi	0
+289	22134 - LP1-FPUNA-2015	0
+290	fast fourier	0
+291	codeforces div 1	0
+292	interval tree	0
+293	bit mask	0
+294	recursive	0
+295	a2	0
+296	number	0
+297	Problem 2 Component Testing	0
+298	2d arraay	0
+299	rabin	0
+300	basic programming	0
+301	segmentation tree	0
+302	Ahmad	0
+303	rating<1300	0
+304	353	0
+305	primes	0
+306	loops	0
+307	10945	0
+308	dfs bfs	0
+309	priority queue	0
+310	sqrt	0
+311	recurssion	0
+312	binry	0
+313	hossamyosef	0
+314	DIV 2 A	0
+315	training round	0
+316	sub sequence	0
+317	disjoint set	0
+318	palindrome	0
+319	disjoint union	0
+320	niquefa	0
+321	div3	0
+322	473	0
+323	multiset	0
+324	Partial sum	0
+325	sum	0
+326	lucas theorem	0
+327	quick sort	0
+328	fractal	0
+329	Div 2 B	0
+330	acmASCIS Level 1 Training '19	0
+331	suffix	0
+332	codeforce div1 catagori A	0
+333	Codeforces Div.2-A Problems	0
+334	nfa	0
+335	Consecutive Ranges	0
+336	adhoc	0
+337	Binary sea	0
+338	obi	0
+339	backtacking	0
+340	Mid-Central USA	0
+341	Di	0
+342	Dijkstra dfs bfs	0
+343	Beginer	0
+344	*1000	0
+345	fibo	0
+346	cummulative	0
+347	r	0
+348	prefix sum	0
+349	depth first search	0
+350	A2OJ FCI-CU	0
+351	pair	0
+352	icpc	0
+353	oc	0
+354	Algo	0
+355	Modulo	0
+356	two pointerd	0
+357	digit	0
+358	insertion	0
+359	13	0
+360	linkedlist	0
+361	dynamic programmig	0
+362	tree	0
+363	heap	0
+364	Sweep	0
+365	regions	0
+366	math geometry	0
+367	bs	0
+368	prims	0
+369	Bfs Dfs	0
+370	the life	0
+371	url	0
+372	sliding window	0
+373	data	0
+374	Codeforces Div. 1 - A Problems	0
+375	CACI-2019B	0
+376	toplogical sort	0
+377	fenwick	0
+378	Graphs & DP Div2 D - E Codeforces	0
+379	dy	0
+380	rec	0
+381	acam	0
+382	maps	0
+383	UFG	0
+384	ladder 2a	0
+385	Add them twice	0
+386	permutatiin	0
+387	prefix	0
+388	UNCC ACM	0
+389	counting	0
+390	optimizations	0
+391	matching	0
+392	dynamic problem	0
+393	FortunA	0
+394	acm phase 0	0
+395	dynamic programming,codeforces DIV 2 D	0
+396	str	0
+397	PST Winter Camp	0
+398	knj	0
+399	myDS	0
+400	extended	0
+401	two pointer	0
+402	. Translation	0
+403	factoradic	0
+404	hanoi	0
+405	hash	0
+406	dijstrika	0
+407	A	0
+408	Mo's	0
+409	Div2	0
+410	Lenear Search	0
+411	fenqick tree	0
+412	zu	0
+413	ternary	0
+414	greeady	0
+415	Benha ACM	0
+416	disjoint	0
+417	frequency arrays	0
+418	crazy search	0
+419	Contests	0
+420	biginteger	0
+421	hakenbush	0
+422	Codeforces Div.1 - A problems	0
+423	first	0
+424	zbox	0
+425	div 1-A	0
+426	re	0
+427	noba_96	0
+428	sets	0
+429	ahmeddahi92	0
+430	d	0
+431	pointer	0
+432	art	0
+433	A - Siggy?s sum	0
+434	binary sear	0
+435	PI-UFS-T01	0
+436	puzzles	0
+437	bitmask	0
+438	binary sarch	0
+439	ad hock	0
+440	FAST EXPONENTIATION	0
+441	geomtry	0
+442	6959	0
+443	bellman	0
+444	struct	0
+445	k	0
+446	djikstra	0
+447	sparse	0
+448	exchange argument	0
+449	195	0
+450	number theory problems	0
+451	linked	0
+452	implmentation	0
+453	Dinamic	0
+454	AVL	0
+455	graph theory	0
+456	combinations	0
+457	hash tables	0
+458	codeforces Div 1 A	0
+459	jj	0
+460	floyd	0
+461	java	0
+462	warshall	0
+463	bitmasking	0
+464	s	0
+465	Avanzada	0
+466	bitmask DP	0
+467	binary	0
+468	B	0
+469	articulation	0
+470	fre	0
+471	link list	0
+472	ECPC	0
+473	topo	0
+474	longest common subsequence	0
+475	e	0
+476	web	0
+477	ACM Tunisian Collegiate Programming Contest 2013 & ACM Jordanian Collegiate Programming Contest 2013	0
+478	Iterative Complete Seach	0
+479	easy	0
+480	back track	0
+481	segment	0
+482	sort	0
+483	prime number	0
+484	bitmask problems	0
+485	bitmasks problems	0
+486	begineer	0
+487	1119	0
+488	alwilfigas	0
+489	201	0
+490	binary exponentitation	0
+491	1600-1699	0
+492	2791 - Lagrange's Four-Square Theorem	0
+493	recursively divide and conquer	0
+494	Iona santana	0
+495	breath first search	0
+496	vector	0
+497	jugal	0
+498	Pattern	0
+499	hosam	0
+500	Practice	0
+501	decode	0
+502	floyd warshall	0
+503	searching	0
+504	union find	0
+505	Treinamento1604	0
+506	sub array	0
+507	Div 2	0
+508	159	0
+509	The_Battle	0
+510	comulitive	0
+511	heap sort	0
+512	priority queues	0
+513	dd	0
+514	div	0
+515	floyed	0
+516	greedy problem	0
+517	maths	0
+518	secret word	0
+519	union	0
+520	352-Pesky Palindromes	0
+521	co	0
+522	0/1	0
+523	Contest Hall Preparation	0
+524	North America - Mid-Central USA	0
+525	bit manu	0
+526	classes	0
+527	Seguimiento basicos	0
+528	object oriented	0
+529	comb	0
+530	fermat	0
+531	graphe	0
+532	priority	0
+533	add hoc	0
+534	standard template library	0
+535	Simple Math	0
+536	bin	0
+537	dynamic programing	0
+538	srongly connected component	0
+539	reursion	0
+540	manel maatouk	0
+541	abed	0
+542	bitmas	0
+543	jzzhu and sequences	0
+544	expectation	0
+545	Codeforces Div. 1, E	0
+546	flo	0
+547	graph coloring	0
+548	garf	0
+549	shortestpath	0
+550	floy	0
+551	bridges	0
+552	abdalrahmanatwah	0
+553	SPC	0
+554	chinese	0
+555	crggamers	0
+556	operator overloading	0
+557	matrix	0
+558	euler totient	0
+559	bits	0
+560	dynamic p	0
+561	Chinese Postman Problem	0
+562	region	0
+563	Graph Coloring Welsh-Powell Algorithm	0
+564	dynamic	0
+565	arabella	0
+566	longest	0
+567	Div.2-C	0
+568	15822	0
+569	condations	0
+570	29708 - ContestPreparation	0
+571	0-1 BFS	0
+572	function	0
+573	eol	0
+574	game	0
+575	LCIS	0
+576	window sliding	0
+577	cumulative	0
+578	arab	0
+579	SRp2019	0
+580	pinary search	0
+581	LP1-I_so_love_programming!	0
+582	Bitmanipulation	0
+583	backtrack	0
+584	binarysearch	0
+585	koki	0
+586	convex	0
+587	greedy and dp	0
+588	breadth first search	0
+589	fast matrix exponentiation	0
+590	graph matching	0
+591	coci	0
+592	set	0
+593	floodfill	0
+594	contiguous longest subarray	0
+595	252	0
+596	topcoder	0
+597	structs	0
+598	arithmetic	0
+599	customfurnish	0
+600	basic	0
+601	bisection	0
+602	max flow	0
+603	disjoint ser	0
+604	1	0
+605	game thery	0
+606	Codeforces Div.1-A Problems	0
+607	backtraking	0
+608	ST;	0
+609	puzz	0
+610	184. Strings with Same Letters	0
+611	2-sat	0
+612	modular arithmetic	0
+613	books	0
+614	bina	0
+615	Judging Troubles	0
+616	MaratonIME	0
+617	z	0
+618	knapsack algorithm	0
+619	acm sham	0
+620	Windows	0
+621	map	0
+622	divide	0
+623	rolling hash	0
+624	Recordare	0
+625	india	0
+626	seive	0
+627	184	0
+628	ACM Arab Collegiate Programming Contest 2013	0
+629	c	0
+630	pst	0
+631	sorting	0
+632	codefoces	0
+633	vec	0
+634	inter	0
+635	Ladder	0
+636	np	0
+637	Subarray	0
+638	graps	0
+639	codeforce A	0
+640	recurion	0
+641	ninary	0
+642	SAMO	0
+643	ent	0
+644	Media	0
+645	repeti?ao	0
+646	dp bitmask	0
+647	adhocv	0
+648	stack	0
+649	problem solving C++	0
+650	linked lists	0
+651	codeforces Div2 A	0
+652	adoc	0
+653	meet-in-the-middle	0
+654	iiitd	0
+655	binary tree	0
+656	simple	0
+657	Mother bear uva	0
+658	Legendre?s formula	0
+659	ifrn	0
+660	list	0
+661	Complete Search	0
+662	Mriganga	0
+663	square root	0
+664	array	0
+665	Palindromes	0
+666	sweep line	0
+667	training round #2	0
+668	python	0
+669	gemastik	0
+670	geekhamza	0
+671	thenoobestever	0
+672	sara reda	0
+673	ioi	0
+674	30008	0
+675	accum	0
+676	upper bound	0
+677	Theatre Square	0
+678	flow lower bound	0
+679	biney index tree	0
+680	facil	0
+681	Alisson viana	0
+682	m	0
+683	Gammal	0
+684	gallup poll	0
+685	red	0
+686	euclid	0
+687	regionals	0
+688	trre	0
+689	twins	0
+690	Implicit	0
+691	a canh	0
+692	palan	0
+693	probability	0
+694	cum	0
+695	SERCLE DEATH	0
+696	Binary Search Trees	0
+697	Educational	0
+698	BIT	0
+699	D-Codeforces	0
+700	cumulative sum	0
+701	bit masking	0
+702	125. Objects Count	0
+703	impementation	0
+704	stacks	0
+705	Iniciante - Aracode	0
+706	codeforces div.3	0
+707	codeforces div 1-a	0
+708	recurrsion	0
+709	genmetry	0
+710	Junior Training Sheet - template - v4.4	0
+711	recursiom	0
+712	kadane	0
+713	Sparse Table	0
+714	Sheet: https://a2oj.com/register?ID=32538	0
+715	manacher algorithm	0
+716	https://a2oj.com/category?ID=408	0
+717	Math & Geometry	0
+718	number system	0
+719	prime palindrome	0
+720	shop	0
+721	mineira	0
+722	Div 3	0
+723	ternary search	0
+724	hbb	0
+725	subsequences	0
+726	topological sory	0
+727	meet	0
+728	batrr	0
+729	combination	0
+730	timus	0
+731	bainary search	0
+732	Groups	0
+733	Joud Khattab	0
+734	ensit	0
+735	ad	0
+736	permutation	0
+737	Contest 1	0
+738	tarjan	0
+739	dynaamic	0
+740	2A	0
+741	Rabin-kurp	0
+742	Combi	0
+743	N Queen problem	0
+744	arrey	0
+745	Capital City	0
+746	Water	0
+747	1600	0
+748	Topological sort	0
+749	pattern search	0
+750	caoyu01	0
+751	chinese remainder	0
+752	impemention	0
+753	bipartite	0
+754	Translation	0
+755	1160	0
+756	rescursion	0
+757	JCPC	0
+758	range minimum querry	0
+759	acpc	0
+760	search	0
+761	35936 - EWU_CSE 105_Spring 2018_Section 6_Lab Contest 1	0
+762	binary index tree	0
+763	spoj	0
+764	Acm world final 2011	0
+765	mobbius	0
+766	strongly connected	0
+A2OJ © Copyright 2010-2019 Ahmed Aly All Rights Reserved. A2OJ uses Sphere Engine™ © by Sphere Research Labs.
+
+
+
+CS.Acadmy
+Ad Hoc
+Backtracking
+Binary Search
+Bits
+Bitset
+Bitwise Operations
+Gray Codes
+Constructive Algorithms
+Data Structures
+Binary Search Tree
+Deque
+Disjoint Sets
+Fenwick Tree
+Hashing
+Hashmap
+Heap
+Linked Lists
+Range Minimum Query
+Segment Tree
+Stack
+Trie
+Divide and Conquer
+Dynamic Programming
+Exponential States
+Matrix Exponentiation
+Tree Dynamic Programming
+Enforced Template
+Game Theory
+Nim
+Sprague Grundy
+Geometry
+Convex Hull
+Sweep Line (Geometry)
+Graphs
+BFS
+DFS
+Dijkstra
+Flood Fill
+Maximum Flow
+Maximum Independent Set
+Maximum Matching
+Strongly Connected Components
+Topological Sorting
+Greedy
+Implementation
+Interactive
+Adaptive
+Iteration
+Sweep Line (Iteration)
+Two Pointers
+Math
+Burnside's Lemma
+Combinatorics
+Expected Value
+FFT
+Gaussian Elimination
+Greatest Common Divisor
+Permutation
+Pigeonhole Principle
+Sieve of Eratosthenes
+Recursion
+Sorting
+Median
+Sqrt Decomposition
+Strings
+KMP
+Longest Common Prefix
+Longest Common Suffix
+Z Algorithm
+Ternary Search
+Trees
+Centroid Decomposition
+Lowest Common Ancestor

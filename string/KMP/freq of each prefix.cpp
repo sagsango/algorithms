026e4e9edf99202b0ff320f75c76e86dce00363f
Sagar Singh <34793802+sagsango@users.noreply.github.com>
2019-11-26 17:24:38 +0530

    Update freq of each prefix.cpp

--- string/KMP/freq of each prefix.cpp
+++ string/KMP/freq of each prefix.cpp
@@ -1,5 +1,18 @@
 Counting the number of occurrences of each prefix
 
+vector<int> prefix_function(string s) {
+    int n = (int)s.length();
+    vector<int> pi(n);
+    for (int i = 1; i < n; i++) {
+        int j = pi[i-1];
+        while (j > 0 && s[i] != s[j])
+            j = pi[j-1];
+        if (s[i] == s[j])
+            j++;
+        pi[i] = j;
+    }
+    return pi;
+}
 
 vector<int> freq_of_prefix(string s)
     { 

0b04e74411333b661647dc1f528d9ff66646479e
Sagar Singh <34793802+sagsango@users.noreply.github.com>
2019-11-26 21:32:42 +0530

    Create Application.cpp

---
+++ string/Suffix Array/Application.cpp
@@ -0,0 +1,3 @@
+1. Finding the smallest cyclic shift  = suffixarray[0];
+2. Finding a substring in a string    = O( |t| * log|t|  + |p| * log|t| )
+                                      = suffix array formtation of text + binary search on suffix array for text

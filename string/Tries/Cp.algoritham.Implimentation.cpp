fa953f4b3039ffad2fcb5b57487d3915f1a6a798
Sagar Singh <34793802+sagsango@users.noreply.github.com>
2019-11-28 21:51:42 +0530

    Create Cp.algoritham.Implimentation.cpp

---
+++ string/Tries/Cp.algoritham.Implimentation.cpp
@@ -0,0 +1,26 @@
+Source   :   https://cp-algorithms.com/string/aho_corasick.html
+
+const int K = 26;
+struct Vertex {
+    int next[K];
+    bool leaf = false;
+
+    Vertex() {
+        fill(begin(next), end(next), -1);
+    }
+};
+
+vector<Vertex> trie(1);
+
+void add_string(string const& s) {
+    int v = 0;
+    for (char ch : s) {
+        int c = ch - 'a';
+        if (trie[v].next[c] == -1) {
+            trie[v].next[c] = trie.size();
+            trie.emplace_back();
+        }
+        v = trie[v].next[c];
+    }
+    trie[v].leaf = true;
+}

58786fb5d96b4ba932d7aff2806e219c01735c5f
Sagar Singh <34793802+sagsango@users.noreply.github.com>
2019-12-02 09:29:18 +0530

    Create Diameter of Tree .cpp

---
+++ graph/Diameter of Tree .cpp
@@ -0,0 +1,19 @@
+1. Using DP on Trees 
+2. 2 DFS
+
+1. Find maxDia at  every v belongs to tree   // DP on Trees
+   int dfsForDia(int u,int p,int &maxDia)
+   {
+      vector<int>childDis;
+      for(auto v:g[u])
+      if(v!=p)childDis.push_back(dfsForDia(v,u,maxDia);
+      sort(childDis.rbegin(),childDis.rend());
+      if(childDis.size() >= 2)maxDia=max(maxDia,childDis[0]+childDis[1]);
+      else if(childDis.size()==1)maxDia=max(maxDia,childDis[0]);
+      return 1 + (childDis.size()?childDis[0]:0);
+   }
+
+2. let a<------>b be the diameter
+   then maximum distance  node from every node will be one of the a or b  [ so get a or b using 1st dfs ]
+   now maximum distance node from a/b will be b/a [ so get both a and b using 2nd dfs ]
+   

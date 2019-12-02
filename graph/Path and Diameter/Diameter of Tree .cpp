157f57c23badf21c770ac056e85e302cfbb14b31
Sagar Singh <34793802+sagsango@users.noreply.github.com>
2019-12-02 13:39:25 +0530

    Update Diameter of Tree .cpp

--- graph/Path and Diameter/Diameter of Tree .cpp
+++ graph/Path and Diameter/Diameter of Tree .cpp
@@ -1,7 +1,10 @@
+-- Referance  : Competitive Programmer’s Handbook Antti Laaksonen Draft July 3, 2018  [ Page - 137 ]
 1. Using DP on Trees 
 2. 2 DFS
 
-1. Find maxDia at  every v belongs to tree   // DP on Trees
+1. Find maxDia at  every v belongs to tree   
+   // DP on Trees
+   // Need only 2 max values at evey node
    int dfsForDia(int u,int p,int &maxDia)
    {
       vector<int>childDis;
@@ -15,5 +18,5 @@
 
 2. let a<------>b be the diameter
    then maximum distance  node from every node will be one of the a or b  [ so get a or b using 1st dfs ]
-   now maximum distance node from a/b will be b/a [ so get both a and b using 2nd dfs ]
+   now maximum distance node from a/b will be b/a                         [ so get both a and b using 2nd dfs ]
    

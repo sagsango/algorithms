199797361be7d8ebbc1042b9ef12e341703859d6
Sagar Singh <34793802+sagsango@users.noreply.github.com>
2019-12-02 16:07:48 +0530

    Update and rename minimum d(u,v).cpp to minimum d(s,t).cpp

---
+++ graph/DP on DAG/minimum d(s,t).cpp
@@ -0,0 +1,24 @@
+/*recursive*/
+vector<int>dis(N+1,inf);
+dis[s]=0;
+int min_dis(int s,int t)
+{
+    /* It will be more optimal is there is only one query , because it will do not do extra work */
+     if( s == t )return 0;
+     for(auto [v,w]:g[t])
+         dis[t]=min(dis[t],dis(s,v)+w);
+     return dis[t];
+}
+   
+/*iterative*/
+int min_dis(int s,int t)
+{
+    /*after running the algo , dis[v] contains min_dis(s,v) for every v */
+     vector<int>dis(N+1,inf);
+     topo_sort();
+     dis[s]=0;
+     for(int i=0;i<topo.size();i++)
+        for(auto [v,w]:g[topo[i]])
+            dis[v]=min(dis[v],dis[ topo[i] ]+w);
+     return dis[t];
+}

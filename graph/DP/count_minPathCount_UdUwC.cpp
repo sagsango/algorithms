ba0c5fd86e49efe50ea971e8b906c0b5b9dca8e3
Sagar Singh <34793802+sagsango@users.noreply.github.com>
2019-12-02 17:15:56 +0530

    Create count_minPathCount_UdUwC.cpp

---
+++ graph/DP/count_minPathCount_UdUwC.cpp
@@ -0,0 +1,34 @@
+source   :   https://www.geeksforgeeks.org/number-shortest-paths-unweighted-directed-graph/
+
+vector<int>dis(N+1,inf);
+vector<int>cnt(N+1,0);
+vector<int>vis(N+1);
+int count_minPath(int s,int v)
+{
+    queue<int>q;
+    dis[s]=0;
+    cnt[s]=1;
+    vis[s]=1;
+    q.push(s);
+    while( q.size() )
+    {
+        int u=q.top();q.pop();
+        for(auto v:g[u])
+        {
+              if(!vis[v])
+              {
+                  vis[v]=1;
+                  q.push(v);
+              }
+              if(dis[v]>dis[u]+1)
+              {
+                  dis[v]=dis[u]+1;
+                  cnt[v]=cnt[u];
+              }
+              else if(dis[v]==dis[u]+1)
+                  cnt[v]+=cnt[u];
+        }
+     }
+     return dis[t];
+ }
+              

9b9e19cd329fc836bf11dcb8bad9adf81b75e0d7
Sagar Singh <34793802+sagsango@users.noreply.github.com>
2019-12-03 17:32:27 +0530

    Create Kth ancestor.cpp

---
+++ graph/Query on Trees/Kth ancestor.cpp
@@ -0,0 +1,27 @@
+/* what will be kth ancestor of the node v : log(n) */
+
+const int L=30;
+const int N=1e6;
+vector<int>g[N+1];
+vector<vector<int>>up(N+1,vector<int>(L+1));
+
+int dfs(int u,int p)
+{
+    up[u][0]=p;
+    for(int i=1;i<=L;i++)
+      up[u][i]=up[ up[u][i-1] ][i-1];
+    for(auto v:g[u])
+      if(v!=p)dfs(v,u);
+}
+
+int kth_ancestor(int u,int k)
+{
+    if(!k)return u;
+    for(int i=L;i>=0;i--)
+    if(k - (1LL<<i) &&  up[u][i])
+    {
+          u=up[u][i];
+          k-=1LL<<i;
+    }
+    return up[u][0];
+}

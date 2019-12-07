0bc9e6fe07c8bbffa27f950b346e40b980f0eed1
Sagar Singh <34793802+sagsango@users.noreply.github.com>
2019-12-07 18:28:55 +0530

    Update Links.cpp

--- Seive/Links.cpp
+++ Seive/Links.cpp
@@ -1,1 +1,18 @@
 see codeforces blogs and codechef problems.
+see block seive in cp.algoritham
+see segmented seive.
+  
+ make file for -
+   total no of prime dvisors  multiple and single query.
+   all prime divisors in single query and multiple query.
+   smaest prime divisors for single and multiple query. 
+   no of prime divisors using smallet prime divisors .
+     
+   no of all divisors for multiple and single query.
+   no of all divisors using smallest prime divisor.
+   all divisors in single and multiple query.
+     
+   if n=p1^a x p2^b x p3^c x ....  then find (a + b + c + .... ) using modification in seive.
+   exponet of prime p in n!.
+   
+   

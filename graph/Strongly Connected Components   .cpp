b06a6afa27efb4415422d51308af965e2516ca03
Sagar Singh <34793802+sagsango@users.noreply.github.com>
2019-12-08 21:18:31 +0530

    Update Strongly Connected Components   .cpp

--- graph/Strongly Connected Components   .cpp
+++ graph/Strongly Connected Components   .cpp
@@ -1,3 +1,10 @@
+https://cp-algorithms.com/graph/depth-first-search.html
+Find strongly connected components in a directed graph:
+First do a topological sorting of the graph. Then transpose the graph and run another series of depth first searches 
+in the order defined by the topological sort. For each DFS call the component created by it is a strongly connected component.
+
+------------------------------------------------------------------------------------------------------------------------------
+
 In directed graph set of verticies which are reachable from eath others , forms strongly connected components.
 
 we reverse the direction of the edges .

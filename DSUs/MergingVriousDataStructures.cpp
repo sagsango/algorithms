6729ab7b9635a96c99429bc787bc1bd9b052cbf4
Sagar Singh <34793802+sagsango@users.noreply.github.com>
2019-12-11 07:55:44 +0530

    Update MergingVriousDataStructures.cpp

--- DSUs/MergingVriousDataStructures.cpp
+++ DSUs/MergingVriousDataStructures.cpp
@@ -1,6 +1,18 @@
 source       :    https://cp-algorithms.com/data_structures/disjoint_set_union.html
 Application  :    we are given a tree, each leaf has a number assigned (same number can appear multiple times on different   
                   leaves). We want to compute the number of different numbers in the subtree for every node of the tree.
+                     
+                  This case you can use set at every vertex and do dfs and after exploring child you have to merge all child
+                  container then at this node you can give answer query offline.
+                  psedo code :-
+                  dfs(int u,int p)
+                  {
+                      for(auto v:g[u])
+                        if(v!=p)dfs(v,u),merge(u,v);
+                      ans[query.index]=st[u].size();
+                  }
+ 
+Example      :
   
 
 vector<int> lst[MAXN];

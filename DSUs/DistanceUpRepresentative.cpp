92bf92077b82c9ca455af06b1884ad9de6e5b4e1
Sagar Singh <34793802+sagsango@users.noreply.github.com>
2019-12-11 07:25:51 +0530

    Update DistanceUpRepresentative.cpp

--- DSUs/DistanceUpRepresentative.cpp
+++ DSUs/DistanceUpRepresentative.cpp
@@ -1,1 +1,28 @@
-https://cp-algorithms.com/data_structures/disjoint_set_union.html
+source       :      https://cp-algorithms.com/data_structures/disjoint_set_union.html
+
+
+void make_set(int v) {
+    parent[v] = make_pair(v, 0);
+    rank[v] = 0;
+}
+
+pair<int, int> find_set(int v) {
+    if (v != parent[v].first) {
+        int len = parent[v].second;
+        parent[v] = find_set(parent[v].first);    /*   No path compression is done here   */
+        parent[v].second += len;
+    }
+    return parent[v];
+}
+
+void union_sets(int a, int b) {
+    a = find_set(a).first;
+    b = find_set(b).first;
+    if (a != b) {
+        if (rank[a] < rank[b])      /*  In some case you may need just make a as parent so can not union by rank   */
+            swap(a, b);
+        parent[b] = make_pair(a, 1);
+        if (rank[a] == rank[b])
+            rank[a]++;
+    }
+}

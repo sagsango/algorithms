2987aa0994872290f3ee691a536dd8ba4bca8ece
Sagar Singh <34793802+sagsango@users.noreply.github.com>
2020-01-13 17:05:36 +0530

    Update Kadens.cpp

--- Dynamic Programming/Kadens.cpp
+++ Dynamic Programming/Kadens.cpp
@@ -1,5 +1,5 @@
 //  https://codeforces.com/contest/1285/submission/68510783
-int mxsum = -1e18;
+                int mxsum = -1e18;
 		int mxsofar = 0;
 		for(int i=0;i<n;i++){
 			mxsofar+=v[i];
@@ -8,3 +8,20 @@ int mxsum = -1e18;
 				mxsofar = 0;
 			}
 		}
+
+
+
+
+// two pointers
+int mxsum = -1e18;
+int currsum = 0;
+int l=0;
+for(int i=0;i<n;i++){
+	currsum+=arr[i];
+	mxsum=max(mxsum,currsum);
+	while( j < i && currsum < 0)
+	{
+		currsum-=arr[j];
+		j++;
+	}
+}

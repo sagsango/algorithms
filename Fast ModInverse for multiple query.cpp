d0e3e5279cec3f5a6dc7ba8df251b737e4dc82dc
Sagar Singh <34793802+sagsango@users.noreply.github.com>
2020-01-18 22:26:51 +0530

    Update Fast ModInverse for multiple query.cpp

--- Fast ModInverse for multiple query.cpp
+++ Fast ModInverse for multiple query.cpp
@@ -1,1 +1,2 @@
 https://www.hackerearth.com/practice/math/combinatorics/basics-of-combinatorics/practice-problems/algorithm/bob-and-internship/
+https://www.hackerearth.com/practice/math/combinatorics/basics-of-combinatorics/practice-problems/algorithm/k-special-cells-93550252/

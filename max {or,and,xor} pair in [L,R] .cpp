28cedf96194c0d05821e9b5fd76d3bc9d0788e79
Sagar Singh <34793802+sagsango@users.noreply.github.com>
2020-01-25 18:07:07 +0530

    Update max {or,and,xor} pair in [L,R] .cpp

--- max {or,and,xor} pair in [L,R] .cpp
+++ max {or,and,xor} pair in [L,R] .cpp
@@ -1,8 +1,8 @@
-We want to choose exactly two integers (not necessarily distinct) from the interval [L,R]. 
 1:Find the maximum possible value of the bitwise OR of the chosen integers
 2:Find the maximum possible value of the bitwise XOR of the chosen integers
 3:Find the maximum possible value of the bitwise AND of the chosen integers
 
+#We want to choose exactly two integers (not necessarily distinct) from the interval [L,R]. 
 sol1:
    From MSB
    Untill bits are same in l and r ,ans bit be same
@@ -43,5 +43,60 @@ sol2:
     cout<<max( r & (r-1) , (r-1)&(r-2) );
     https://www.geeksforgeeks.org/maximum-bitwise-and-pair-from-given-range/
    
+
+#we want two distint elemnts in the array.
+sol1.
+sol2. see tri section  in datastructure.
+sol3. https://www.geeksforgeeks.org/maximum-value-pair-array/
+
+// CPP Program to find maximum AND value of a pair 
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// Utility function to check number of elements 
+// having set msb as of pattern 
+int checkBit(int pattern, int arr[], int n) 
+{ 
+	int count = 0; 
+	for (int i = 0; i < n; i++) 
+		if ((pattern & arr[i]) == pattern) 
+			count++; 
+	return count; 
+} 
+
+// Function for finding maximum and value pair 
+int maxAND (int arr[], int n) 
+{ 
+	int res = 0, count; 
+
+	// iterate over total of 30bits from msb to lsb 
+	for (int bit = 31; bit >= 0; bit--) 
+	{ 
+		// find the count of element having set msb 
+		count = checkBit(res | (1 << bit),arr,n); 
+
+		// if count >= 2 set particular bit in result 
+		if ( count >= 2 )		 
+			res |= (1 << bit);		 
+	} 
+
+	return res; 
+} 
+
+// Driver function 
+int main() 
+{ 
+	int arr[] = {4, 8, 6, 2}; 
+	int n = sizeof(arr)/sizeof(arr[0]); 
+	cout << "Maximum AND Value = " << maxAND(arr,n); 
+	return 0; 
+} 
+
    
+
+# max or value with x :
+                gievn x and array;
+                fill all the array in tri
+		now serah max or with n*logn complexity.
+
    

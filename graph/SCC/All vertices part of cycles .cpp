73e5686e9185a7ec8f174d2cc1ae383f4adb65ee
Sagar Singh <34793802+sagsango@users.noreply.github.com>
2020-03-17 02:31:02 +0530

    Update All vertices part of cycles .cpp

--- graph/SCC/All vertices part of cycles .cpp
+++ graph/SCC/All vertices part of cycles .cpp
@@ -94,3 +94,79 @@ using namespace std;
         cout << cnt << endl;
 	}
   }
+
+
+
+
+
+
+Alternate Solution :
+/* All the v have only one child so part_of_cycle[v] will be only one times so this is good TL */
+#include<bits/stdc++.h>
+#define int long long 
+using namespace std;
+
+vector < vector<int> > g;
+vector < int > clr;
+vector < int > part_of_cycle;
+vector < int > par;
+
+void dfs(int u ,int p )
+{
+	clr[u]=1;
+	par[u]=p;
+	for(auto v:g[u])
+	{
+		if(!clr[v])
+		{
+			dfs(v,u);
+		}
+		else if( clr[v]==1 )
+		{
+			int x = u ; 
+			while( x!= v)
+			{
+				part_of_cycle[x]=1;
+				x=par[x];
+			}
+			part_of_cycle[x]=1;
+		}
+	}
+	clr[u]=2;
+}
+int32_t main()
+{
+	int q;cin>>q;
+	while(q--)
+	{
+		int n;cin>>n;
+		g = vector<vector<int>>(n+1);
+		clr = vector<int>(n+1);
+		part_of_cycle = vector<int>(n+1);
+		par = vector<int>(n+1);
+	
+		for(int i=0;i<n;i++)
+		{
+			int d ; cin >> d;
+			int a = i ;
+			int b = ( a + d +1)%n;
+			
+			g[a+1].push_back(b+1);
+		}
+		
+		for(int i=1;i<=n;i++)
+		{
+			if(!clr[i])
+			{
+				dfs(i,0);
+			}
+		}
+		
+		int cnt = 0;
+		for(int i=1;i<=n;i++)
+		cnt+=part_of_cycle[i];
+		cout << cnt << endl;
+	}
+}
+		
+

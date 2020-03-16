b2391e75370e002a6eea1a67aa47c0f9b9fb53e3
Sagar Singh <34793802+sagsango@users.noreply.github.com>
2020-03-16 23:12:35 +0530

    Create Implimentstion.cpp

---
+++ graph/SCC/Implimentstion.cpp
@@ -0,0 +1,52 @@
+source code   :   https://cp-algorithms.com/graph/strongly-connected-components.html
+
+
+
+vector < vector<int> > g, gr;
+    vector<bool> used;
+    vector<int> order, component;
+
+    void dfs1 (int v) {
+        used[v] = true;
+        for (size_t i=0; i<g[v].size(); ++i)
+            if (!used[ g[v][i] ])
+                dfs1 (g[v][i]);
+        order.push_back (v);
+    }
+
+    void dfs2 (int v) {
+        used[v] = true;
+        component.push_back (v);
+        for (size_t i=0; i<gr[v].size(); ++i)
+            if (!used[ gr[v][i] ])
+                dfs2 (gr[v][i]);
+    }
+
+    int main() {
+        int n , m ;
+        cin >> n >> m;
+        g=vector<vector<int>>(n);
+        gr=vector<vector<int>>(n);
+        used=vector<int>(n);
+        
+        for (int i = 0 ;i < m;i++) {
+            int a, b;
+            cin >> a >> b;
+            g[a].push_back (b);
+            gr[b].push_back (a);
+        }
+
+        used.assign (n, false);
+        for (int i=0; i<n; ++i)
+            if (!used[i])
+                dfs1 (i);
+        used.assign (n, false);
+        for (int i=0; i<n; ++i) {
+            int v = order[n-1-i];
+            if (!used[v]) {
+                dfs2 (v);
+                ... printing next component ...
+                component.clear();
+            }
+        }
+    }

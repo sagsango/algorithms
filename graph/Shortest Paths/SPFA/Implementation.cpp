f1b4b8939b9004953e755d73a9a7cd509ba80ef5
Sagar Singh <34793802+sagsango@users.noreply.github.com>
2020-03-21 03:32:57 +0530

    Create Implementation.cpp

---
+++ graph/Shortest Paths/SPFA/Implementation.cpp
@@ -0,0 +1,39 @@
+//   https://cp-algorithms.com/graph/bellman_ford.html
+//   The worst case of this algorithm is equal to the O(nm) of the Bellman-Ford, but in practice it works much faster and some people claim that it works even in O(m) on average. However be careful, because this algorithm is deterministic and it is easy to create counterexamples that make the algorithm run in O(nm).
+//   With negative weigth [ No negative cycle ]
+const int INF = 1000000000;
+vector<vector<pair<int, int>>> adj;
+
+bool spfa(int s, vector<int>& d) {
+    int n = adj.size();
+    d.assign(n, INF);
+    vector<int> cnt(n, 0);
+    vector<bool> inqueue(n, false);
+    queue<int> q;
+
+    d[s] = 0;
+    q.push(s);
+    inqueue[s] = true;
+    while (!q.empty()) {
+        int v = q.front();
+        q.pop();
+        inqueue[v] = false;
+
+        for (auto edge : adj[v]) {
+            int to = edge.first;
+            int len = edge.second;
+
+            if (d[v] + len < d[to]) {
+                d[to] = d[v] + len;
+                if (!inqueue[to]) {
+                    q.push(to);
+                    inqueue[to] = true;
+                    cnt[to]++;
+                    if (cnt[to] > n)
+                        return false;  // negative cycle
+                }
+            }
+        }
+    }
+    return true;
+}

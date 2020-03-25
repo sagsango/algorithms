c96535fb91dbb2d09596f57d5bd2b879a04f87c9
Sagar Singh <34793802+sagsango@users.noreply.github.com>
2020-03-25 07:29:53 +0530

    Update implementation.cpp

--- graph/Shortest Paths/Floyd-Warshell/implementation.cpp
+++ graph/Shortest Paths/Floyd-Warshell/implementation.cpp
@@ -5,16 +5,17 @@ d[N][N] : D[i][i] = 0;
           D[i][j] = w;
 p[N][N] : p[i][j] = k;[path throught]
 
-for(int i=1;i<=n;i++)
+for(int i=1;i<=n;i++) // adding one vertex one by one
 {
       for(int j=1;j<=n;j++)
       {
             for(int k=1;k<=n;k++)
             {
-                  if( d[i][k] > d[i][j] + d[j][k] )
+            /* changing order of loop give WA */
+                  if( d[j][k] > d[j][i] + d[i][k] ) // relax all path passing treough i
                   {
-                        d[i][k]=d[i][j]+d[j][k];
-                        p[i][k]=j;
+                        d[j][k]=d[j][i]+d[i][k];
+                        p[j][k]=i;
                   }
             }
       }

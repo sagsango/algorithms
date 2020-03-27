86fb36cbd447825f50b918effa140c8edd3577cc
Sagar Singh <34793802+sagsango@users.noreply.github.com>
2020-03-27 18:05:31 +0530

    Update LAC implimentation.cpp

--- graph/LCA/LAC implimentation.cpp
+++ graph/LCA/LAC implimentation.cpp
@@ -6,12 +6,12 @@ Solution  :  https://codeforces.com/gym/272284/submission/73340387
 #define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 using namespace std;	
 	
-const int N = 1e5 , M = 20 ;
+const int N = 1e5 , lgN = 20 ;
 vector<int> g[N+1];
 vector<int> tin(N+1);
 vector<int> tout(N+1);
 vector<int> h(N+1);
-vector<vector<int>>up(N+1,vector<int>(M+1));
+vector<vector<int>>up(N+1,vector<int>(lgN+1));
 int t=0 , n ;
 
 void dfs(int u,int p)
@@ -19,7 +19,7 @@ void dfs(int u,int p)
 	tin[u]=++t;
 	h[u]=h[p]+1;
 	up[u][0]=p;
-	for(int i=1;i<=M;i++)
+	for(int i=1;i<=lgN;i++)
 	up[u][i]=up[ up[u][i-1] ][i-1];
 	
 	for(auto v:g[u])if(v!=p)dfs(v,u);
@@ -37,7 +37,7 @@ int LCA(int u,int v)
 	if( isAncestor(u,v) )return u;
 	if( isAncestor(v,u) )return v;
 	
-	for(int i=M;i>=0;i--)
+	for(int i=lgN;i>=0;i--)
 	if( up[u][i] && !isAncestor(up[u][i],v))
 	u=up[u][i];
 	

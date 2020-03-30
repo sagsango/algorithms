d5f64b47a833f8495060bdc52ecc3663d4cb7b51
Sagar Singh <34793802+sagsango@users.noreply.github.com>
2020-03-30 13:26:40 +0530

    Update lca.cpp

--- graph/Binary Lifting/lca.cpp
+++ graph/Binary Lifting/lca.cpp
@@ -1,46 +1,44 @@
-int n, l;
-vector<vector<int>> adj;
+vector<vector<pair<int,int>>>G;
+vector<int> tin;
+vector<int> tout;
+vector<vector<int>>up;
+int t;
 
-int timer=0;
-vector<int> tin, tout;
-vector<vector<int>> up;
-
-void dfs(int v, int p)
+void dfs(int u,int p,int w)
 {
-    tin[v] = ++timer;
-    up[v][0] = p;
-    for (int i = 1; i <= l; ++i)
-        up[v][i] = up[up[v][i-1]][i-1];
-
-    for (int u : adj[v]) {
-        if (u != p)
-            dfs(u, v);
-    }
-
-    tout[v] = ++timer;
+	tin[u]=++t;
+	up[u][0]=p;
+	for(int i=1;i<=lgN;i++)
+	{
+		up[u][i]=up[ up[u][i-1] ][i-1];
+	}
+	for(auto it:G[u])
+	{
+		int v=it.first;
+		int i=it.second;
+		if( v!= p)
+		{
+			dfs(v,u,W[i]);
+		}
+	}
+	tout[u]=++t;
 }
 
-bool is_ancestor(int u, int v)
+int isAncestor(int u,int v)
 {
-    return tin[u] <= tin[v] && tout[u] >= tout[v];
+	return tin[u]<=tin[v] && tout[v]<=tout[u];
 }
 
-int lca(int u, int v)
+int LCA(int u,int v)
 {
-    if (is_ancestor(u, v))
-        return u;
-    if (is_ancestor(v, u))
-        return v;
-    for (int i = l; i >= 0; --i) {
-        if (up[u][i] && !is_ancestor(up[u][i], v))
-            u = up[u][i];
-    }
-    return up[u][0];
+	if( isAncestor(u,v) )return u;
+	if( isAncestor(v,u) )return v;
+	
+	for(int i=lgN;i>=0;i--)
+	if( up[u][i] && !isAncestor(up[u][i],v))
+	u=up[u][i];
+	
+	return up[u][0];
 }
-dfs(1,0);
-
-
-
-
 
 

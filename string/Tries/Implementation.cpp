31eb68317f5f12f8c6ed3408b6e0d7aaccef8bec
Sagar Singh <34793802+sagsango@users.noreply.github.com>
2020-04-06 16:38:21 +0530

    Update and rename Cp.algoritham.Implimentation.cpp to Implementation.cpp

---
+++ string/Tries/Implementation.cpp
@@ -0,0 +1,49 @@
+https://codingcompetitions.withgoogle.com/kickstart/round/000000000019ffc7/00000000001d3ff3
+
+#include<bits/stdc++.h>
+using namespace std;
+struct tri{
+    tri * child[26];
+    int cnt;
+};
+tri *head;
+int n,k;
+void insert(string s)
+{
+    tri * curr = head;
+    for(size_t i=0;i<s.length();i++)
+    {
+        if( !curr->child[s[i]-'A'])curr->child[s[i]-'A']=new tri();
+        curr=curr->child[s[i]-'A'];
+        curr->cnt++;
+    }
+}
+int query(tri*curr)
+{
+    int ans=curr->cnt/k;
+    for(int i=0;i<26;i++)
+    {
+        if(curr->child[i])
+        {
+            ans+=query(curr->child[i]);
+            
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+    int T;cin>>T;
+    for(int t=1;t<=T;t++)
+    {
+        head = new tri();
+        cin>>n>>k;
+        for(int i=0;i<n;i++)
+        {
+            string s;cin>>s;
+            insert(s);
+        }
+        cout <<"Case #"<<t<<": "<< query(head) << endl;
+    }
+}

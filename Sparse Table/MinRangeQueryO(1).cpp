30e793498cba5bbf7c07ea5e458e65a2c26ad74d
Sagar Singh <34793802+sagsango@users.noreply.github.com>
2020-04-09 13:19:54 +0530

    Create MinRangeQueryO(1).cpp

---
+++ Sparse Table/MinRangeQueryO(1).cpp
@@ -0,0 +1,33 @@
+// https://www.spoj.com/problems/RMQSQ/
+#include<bits/stdc++.h>
+#define int long long
+#define endl "\n"
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+using namespace std;
+
+const int maxn = 100005 , lgN = 20;
+vector<vector<int>>dp(maxn,vector<int>(lgN));
+int n;
+int32_t main(){
+	IOS
+	cin >> n ;
+	for(int i=1;i<=n;i++){
+		cin>>dp[i][0];
+	}
+	for(int i=n;i>=1;i--){
+		for(int j=1;i+(1LL<<j)-1<=n;j++){
+			dp[i][j]=min(dp[i][(j-1)],dp[i+(1LL<<(j-1))][(j-1)]);
+		}
+	}
+	
+	
+	
+	int q;cin>>q;
+	while( q-- ){
+		int l,r;cin>>l>>r;
+		l++,r++;
+		int j = log2(r-l+1);
+		cout<<min(dp[l][j],dp[r-(1LL<<j)+1][j])<<endl;
+	}
+}
+

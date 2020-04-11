2b4d35a16d1de40fa1213a86657de98d75bedde7
Sagar Singh <34793802+sagsango@users.noreply.github.com>
2020-04-12 04:02:51 +0530

    Create  MCO16404.cpp

---
+++ Sqrt Decomposition/Problem/MCO16404.cpp
@@ -0,0 +1,5 @@
+https://ideone.com/uhsbEv
+https://ideone.com/9ypOF4
+
+https://www.hackerearth.com/practice/data-structures/advanced-data-structures/trie-keyword-tree/practice-problems/algorithm/xor-path-1-f7009db6/description/
+https://www.hackerearth.com/practice/data-structures/advanced-data-structures/trie-keyword-tree/practice-problems/algorithm/xor-path-1-f7009db6/editorial/

7580772b6c152d97789a87313812b4ae928b898a
Sagar Singh <34793802+sagsango@users.noreply.github.com>
2020-04-17 03:02:54 +0530

    Create a.cpp

---
+++ a.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+#define int long long
+#define endl '\n'
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define bug1( x ) cout << (#x) <<"="<< x << endl;
+#define bug2( x , y ) cout << (#x) <<"="<< (x) << "    " << (#y) << "="<< (y) << endl;
+#define bug3( x , y , z ) cout << (#x) <<"="<<(x) << "    " << (#y) <<"="<< (y) << "    " << (#z) <<"="<< (z) << endl;
+#define bug4( x , y , z , w) cout << (#x) <<"="<<(x) << "    " << (#y) <<"="<< (y) << "    " << (#z) <<"="<< (z) << "    " << (#w) <<"="<< w << endl;
+#define bugn( x , n ){ cout<<(#x)<<":";for(int i=0;i<n;i++)cout<<(#x)<<"["<<i<<"]:"<< x[i] << "    "; cout<<endl;}
+#define bugnm( x , n , m ){ cout<<(#x)<<endl;for(int i=0;i<n;i++){ cout<<"Row #"<<i<<":"; for(int j=0;j<m;j++) cout<<x[i][j]<<"     "; cout << endl;}}
+using namespace std;	
+
+
+int32_t main()
+{   
+	IOS
+}
+	
+
+
+
+

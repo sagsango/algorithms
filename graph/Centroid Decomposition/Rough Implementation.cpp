239368c294604ff38323b8c03cdb1b01e3dc1828
Sagar Singh <34793802+sagsango@users.noreply.github.com>
2020-04-28 22:53:33 +0530

    Create Rough Implementation.cpp

---
+++ graph/Centroid Decomposition/Rough Implementation.cpp
@@ -0,0 +1,48 @@
+
+
+void dfs(int u,int par){
+        subtree[u]=1;
+        nodes++;
+        for(auto v:g[u]){
+                if(v==par)continue;
+                dfs(v,u);
+                subtree[u]+=subtree[v];
+        }
+}
+
+
+int center(int u,int par){
+        for(auto v:g[u]){
+                if( v == par ) continue;
+                if( subtree[v] > (nodes>>1) ){
+                        return center(v,u);
+                }
+        }
+        return u;
+}
+        
+
+
+void decompose(int u=1,int par=0){
+
+        /* find centroid of curr subtree */
+        nodes=0;
+        dfs(u,u);
+        int c = center(u,u);
+        centroidpar[c]=par;
+        
+        
+        
+        
+        /* solve here for curr subtree */
+        
+       
+        
+        
+        
+        /* Decomose recursivily and solve rest*/
+        for(auto v:g[c]){
+                g[v].erase(u);
+                decompose(v,c);
+        }
+}

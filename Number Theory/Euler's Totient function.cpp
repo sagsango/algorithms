1e957b76f4cc828db0825d03fefc93c67393cca9
Sagar Singh <34793802+sagsango@users.noreply.github.com>
2020-05-19 22:15:52 +0530

    Create Euler's Totient function.cpp

---
+++ Number Theory/Euler's Totient function.cpp
@@ -0,0 +1,32 @@
+https://cp-algorithms.com/algebra/phi-function.html
+
+void phi_1_to_n(int n) {
+    // using the divisor sum property
+    vector<int> phi(n + 1);
+    phi[0] = 0;
+    phi[1] = 1;
+    for (int i = 2; i <= n; i++)
+        phi[i] = i - 1;
+
+    for (int i = 2; i <= n; i++)
+        for (int j = 2 * i; j <= n; j += i)
+              phi[j] -= phi[i];
+}
+
+
+
+
+void phi_1_to_n(int n) {
+    vector<int> phi(n + 1);
+    phi[0] = 0;
+    phi[1] = 1;
+    for (int i = 2; i <= n; i++)
+        phi[i] = i;
+
+    for (int i = 2; i <= n; i++) {
+        if (phi[i] == i) {
+            for (int j = i; j <= n; j += i)
+                phi[j] -= phi[j] / i;
+        }
+    }
+}

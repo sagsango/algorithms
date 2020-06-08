401fd5ed35679795bc1224e70bbd563cbb3b7a38
Sagar Singh <34793802+sagsango@users.noreply.github.com>
2020-06-08 22:44:58 +0530

    Update rough implementation .cpp

--- graph/Heavy Light Decomposition/rough implementation .cpp
+++ graph/Heavy Light Decomposition/rough implementation .cpp
@@ -1,4 +1,73 @@
-/* see cp.algoritham */
+/* Nothing is Tested */
+void dfs(int u,int p){
+	par[u]=p;
+	subtree[u]=1;
+	for(auto v:g[u]){
+		if( v!=p ){
+			dfs(v,u);
+			subtree[u]+=subtree[v];
+			if( subtree[v] > subtree[ heavy[u] ]){
+				heavy[u]=v;
+			}
+		}
+	}
+}
+void decompose(int u,int p,int h){
+	head[u]=h;
+	pos[u]=++curr_pos;
+	if( heavy[u] )decompose(heavy[u],u,h);
+	for(auto v:g[u]){
+		if( v==p || v == heavy[u] )continue;
+		decompose(v,u,v);
+	}
+}
+
+int pathSum(int u){ // u to root
+	int s = 0;
+	while( u ){
+		d+= query(1,1,n,pos[head[u]],pos[u]);
+		u = par[ head[u] ];
+	}
+	return s;
+}
+
+int pathSum(int u,int l){ // u to lca
+	int d = 0;
+	while( head[l] != head[u] ){
+		d+= query(1,1,n,pos[head[u]],pos[u]);
+		u=par[head[u]];
+	}
+	if( u != l ){
+		d+= query(1,1,n,pos[l],pos[u]);
+	}
+}
+int pathSum(int u,int v){ // u and v
+	while( head[u] != head[v] ){
+		if( dep[head[u]] < dep[head[v]] ){
+			swap(u,v);
+		}
+		d+=query(1,1,n,pos[head[u]],pos[u]);
+		u = par[head[u]];
+	}
+	if( dep[u] < dep[v] ){
+		swap(u,v);
+		d+=query(1,1,n,pos[v],dep[u]);
+	}
+}
+int lca(int u,int v){
+	while( head[u] != head[v] ){
+		if( dep[ head[u] ] < dep[ head[v] ] ){
+			swap(u,v);
+		}
+		u = par[head[u]];
+	}
+	if( dep[u] < dep[v] ){
+		swap(u,v);
+	}
+	return v;
+}
+	
+
 
 //  things matters in life  : head , pos , heavy
 void dfs(int u,int p){

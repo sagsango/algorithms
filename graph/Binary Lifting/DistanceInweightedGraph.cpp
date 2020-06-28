2df7c93786dc59615203befd9ab8c93962cd8fe5
Sagar Singh <34793802+sagsango@users.noreply.github.com>
2020-06-29 00:06:54 +0530

    Update DistanceInweightedGraph.cpp

--- graph/Binary Lifting/DistanceInweightedGraph.cpp
+++ graph/Binary Lifting/DistanceInweightedGraph.cpp
@@ -1,75 +1,59 @@
-vector<vector<pair<int,int>>>G;
-vector<int> tin;
-vector<int> tout;
-vector<vector<int>>up;
-vector<vector<int>>d;
-int t, n , lgN;
+const int maxn = 1e5+10, lgN = 20;
+vector<vector<pair<int,int>>>g(maxn);
+vector<int> tin(maxn);
+vector<int> tout(maxn);
+vector<vector<int>>par(maxn,vector<int>(lgN+1));
+vector<vector<int>>dis(maxn,vector<int>(lgN+1));
+int timer = 0, n;
 
-void dfs(int u,int p,int w)
-{
-	tin[u]=++t;
-	up[u][0]=p;
-	h[u]=h[p]+1;
-	d[u][0]=w;
-	for(int i=1;i<=lgN;i++)
-	{
-		up[u][i]=up[ up[u][i-1] ][i-1];
-		d[u][i]=d[ up[u][i-1] ][i-1]+d[u][i-1];
+void dfs(int u=1,int p=0,int w=0){
+	tin[u]=++timer;
+	par[u][0]=p;
+	dis[u][0]=w;
+	for(int i=1;i<=lgN;i++){
+		par[u][i]=par[ par[u][i-1] ][i-1];
+		dis[u][i]=dis[ par[u][i-1] ][i-1]+dis[u][i-1];
 	}
-	for(auto it:G[u])
-	{
+	for(auto it:g[u]){
 		int v=it.first;
 		int w=it.second;
-		if( v!= p)
-		{
+		if( v!= p){
 			dfs(v,u,w);
 		}
 	}
-	tout[u]=++t;
+	tout[u]=++timer;
 }
 
-int isAncestor(int u,int v)
-{
+int isAncestor(int u,int v){
 	return tin[u]<=tin[v] && tout[v]<=tout[u];
 }
 
-int LCA(int u,int v)
-{
+int lca(int u,int v){
 	if( isAncestor(u,v) )return u;
 	if( isAncestor(v,u) )return v;
-	
-	for(int i=lgN;i>=0;i--)
-	if( up[u][i] && !isAncestor(up[u][i],v))
-	u=up[u][i];
-	
-	return up[u][0];
+	for(int i=lgN;i>=0;i--){
+		if( par[u][i] && !isAncestor(par[u][i],v)){
+			u=par[u][i];
+		}
+	}
+	return par[u][0];
 }
 
-int DisQuery(int u,int v)
-{
-	
-	int l = LCA(u,v);
-	int dis = 0;
-	if( u!=l )
-	{
-		for(int i=lgN;i>=0;i--)
-	    if( up[u][i] && !isAncestor(up[u][i],l))
-	    {
-			dis+=d[u][i];
-			u=up[u][i];
+int disQuery(int u,int v){
+	int l = lca(u,v);
+	int d = 0;
+	auto shiftup =[&](int u){
+		if( u!=l ){
+			for(int i=lgN;i>=0;i--){
+				if( par[u][i] && !isAncestor(par[u][i],l)){
+					d+=dis[u][i];
+					u=par[u][i];
+				}
+			}
+			d+=dis[u][0];
 		}
-		dis+=d[u][0];
-	}
-	if( v!=l )
-	{
-		for(int i=lgN;i>=0;i--)
-	    if( up[v][i] && !isAncestor(up[v][i],l))
-	    {
-			dis+=d[v][i];
-			v=up[v][i];
-		}
-	    dis+=d[v][0];
-	}
-	return dis;
+	};
+	shiftup(u);
+	shiftup(v);
+	return d;
 }
-/////////////////////////////////////////////////////////
